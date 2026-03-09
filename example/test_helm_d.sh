a18fdaa47b35d1ba3753a02fb64c0d16456ddeda
Grant Murphy <grantdillermurphy@gmail.com>
2026-03-09 01:45:25 -0400

    improved evaluation specifications

--- example/test_helm_d.sh
+++ example/test_helm_d.sh
@@ -1,9 +1,11 @@
 lm-eval \
   --model helm_d_115M \
+  --model_args ckpt_dir=... \
   --tasks commonsense_qa,openbookqa,hellaswag \
   --num_fewshot 0
 
 lm-eval \
   --model helm_d_115M \
+  --model_args ckpt_dir=... \
   --tasks mmlu,arc_challenge \
   --num_fewshot 5
--- example/test_mice_120M.sh
+++ example/test_mice_120M.sh
@@ -1,9 +1,11 @@
 lm-eval \
   --model helm_mice_120M \
+  --model_args ckpt_dir=... \
   --tasks commonsense_qa,openbookqa,hellaswag \
   --num_fewshot 0
 
 lm-eval \
   --model helm_mice_120M \
+  --model_args ckpt_dir=... \
   --tasks mmlu,arc_challenge \
   --num_fewshot 5
